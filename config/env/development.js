31b217c95baac87644759dfaff9b9ccc4606dd53
Artem Rossokha <artem.rossokha@iqria.com>
2015-09-21 11:51:16 +0300

    Updates

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
 	db: 'mongodb://localhost/mean-yo-dev',
 	app: {
-		title: 'MEAN - Development Environment'
+		title: 'MEAN Accouning - Dev'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
